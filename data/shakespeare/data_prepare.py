ba48d8ad231b0862e16d97d464559ad2ae2eca6d
fengyang21 <13264396+fengyang21@user.noreply.gitee.com>
2025-03-29 23:25:19 +0800

    first commit

---
+++ data/shakespeare/data_prepare.py
@@ -0,0 +1,50 @@
+import os
+import requests
+import tiktoken
+import numpy as np
+
+# download the tiny shakespeare dataset
+input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
+if not os.path.exists(input_file_path):
+    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
+    with open(input_file_path, 'w', encoding='utf-8') as f:
+        f.write(requests.get(data_url).text)
+
+with open(input_file_path, 'r', encoding='utf-8') as f:
+    data = f.read()
+n = len(data)
+train_data = data[:int(n*0.9)]
+val_data = data[int(n*0.9):]
+# train has 1003854 characters
+# val has 111540 characters
+
+# here are all the unipue characters in the dataset
+chars = sorted(list(set(data)))
+vocab_size = len(chars)
+# print(''.join(chars))
+# print(vocab_size)
+# !$&',-.3:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
+# 65
+
+
+# encode with tiktoken gpt2 bpe
+enc = tiktoken.get_encoding("gpt2")
+train_ids = enc.encode_ordinary(train_data)
+val_ids = enc.encode_ordinary(val_data)
+# train has 301966 tokens
+# val has 36059 tokens
+
+# export to bin files
+train_ids = np.array(train_ids, dtype=np.uint16)
+val_ids = np.array(val_ids, dtype=np.uint16)
+train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
+val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
+# train.bin has 301,966 tokens
+# val.bin has 36,059 tokens
+
+# why to transform to bin files?
+# 1. to save space
+# 2. to speed up loading time
+# 3. to make it easier to shard
+
+
